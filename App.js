5a8f9f6fdc9cf6ebf726500a24d6deddbc11adc6
Arash <arash8877@gmail.com>
2024-04-22 13:18:26 +0200

    added navigators

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import AllExpenses from "./screens/AllExpenses";
+import RecentExpenses from "./screens/RecentExpenses";
+import manageExpense from "./screens/ManageExpense";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ManageExpense" component={manageExpense} />
+          <Stack.Screen name="AllExpenses" component={AllExpenses} />
+          <Stack.Screen name="RecentExpenses" component={RecentExpenses} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

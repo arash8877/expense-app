4627d8761f685ebde9d74690ef368cfa76907762
Arash <arash8877@gmail.com>
2024-04-22 12:52:51 +0200

    created screen components

---
+++ screens/AllExpenses.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+const AllExpenses = () => {
+  return (
+    <text>All Expenses screen</text>
+  )
+}
+
+export default AllExpenses;
---
+++ screens/ManageExpense.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+const manageExpense = () => {
+  return (
+    <text>Manage Expense screen</text>
+  )
+}
+
+export default manageExpense;

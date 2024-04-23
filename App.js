13c81be5b6951c8cb7c3a14906931133b2140330
Arash <arash8877@gmail.com>
2024-04-23 11:52:03 +0200

    modified app to navigate manageExpense screen

--- App.js
+++ App.js
@@ -15,15 +15,22 @@ const BottomTabs = createBottomTabNavigator();
 function ExpenseOverview() {
   return (
     <BottomTabs.Navigator
-      screenOptions={{
+      screenOptions={({ navigation }) => ({
         headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         headerTintColor: "white",
         tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         tabBarActiveTintColor: GlobalStyles.colors.accent500,
         headerRight: ({ tintColor }) => (
-          <IconButton icon="add" size={24} color={tintColor} inPress={()=>{}}/>
+          <IconButton
+            icon="add"
+            size={24}
+            color={tintColor}
+            onPress={() => {
+              navigation.navigate("ManageExpense");
+            }}
+          />
         ),
-      }}
+      })}
     >
       <BottomTabs.Screen
         name="RecentExpenses"
@@ -58,13 +65,20 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+          headerStyle: {backgroundColor: GlobalStyles.colors.primary500},
+          headerTintColor: 'white'
+        }}>
           <Stack.Screen
             name="ExpensesOverview"
             component={ExpenseOverview}
             options={{ headerShown: false }}
           />
-          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} options={{
+            title: 'Manage Expense',
+            headerTitleAlign: 'center',
+            presentation: 'modal'
+          }}/>
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- components/expenses/ExpenseItem.js
+++ components/expenses/ExpenseItem.js
@@ -1,9 +1,14 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 
 const ExpenseItem = ({ description, amount, date }) => {
-  function expensePressHandler() {}
+  const navigation = useNavigation();
+
+  function expensePressHandler() {
+     navigation.navigate('ManageExpense');
+  }
   return (
     <Pressable
       onPress={expensePressHandler}

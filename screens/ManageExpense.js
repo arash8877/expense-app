8620bce5459ae5359966644bab501c45bb425861
Arash <arash8877@gmail.com>
2024-04-24 10:36:34 +0200

    modified manageExpense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,11 +1,18 @@
+import { useLayoutEffect } from "react";
 import { View, Text } from "react-native";
 
-const ManageExpense = ({route}) => {
-  const editedExpenseId = route.params.expenseId;
-  
-  return (
-    <Text>Manage Expense screen</Text>
-  )
-}
+const ManageExpense = ({ route, navigation }) => {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId; //Convert expenseId to boolean
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      //updating of options of a screen inside a screen: navigation.setOptions()
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  return <Text>Manage Expense screen</Text>;
+};
 
 export default ManageExpense;

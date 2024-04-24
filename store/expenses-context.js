abb4ca1d314fc4095849e7ac776da189f24d28f0
Arash <arash8877@gmail.com>
2024-04-24 13:43:31 +0200

    added context api

---
+++ store/expenses-context.js
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+export const ExpensesContext = createContext({
+  expenses: [],
+  addExpense: ({ description, amount, date }) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { description, amount, date }) => {},
+});
+
+
+export const ExpensesContextProvider = ({children}) => {
+    return <ExpensesContext.Provider>
+        {children}
+    </ExpensesContext.Provider>
+}
